208f93272cef9174513be0c59f8048a3d5a523f3
Ruilx <799382072@qq.com>
2025-02-11 00:36:51 +0800

    update

--- src/pydoo/api/db_api.py
+++ src/pydoo/api/db_api.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import abc
+from typing import Literal
 
 
 class DBAPI(object, metaclass=abc.ABCMeta):
-    def __init__(self, config: dict):
-        self.config = config
+    def __init__(self, connection):
+        self.connection = connection
 
     @abc.abstractmethod
     def ping(self):
@@ -25,4 +26,56 @@ class DBAPI(object, metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def rollback(self):
+        raise NotImplementedError
+
+
+class DBAPICursor(object, metaclass=abc.ABCMeta):
+    def __init__(self, cursor):
+        self.cursor = cursor
+
+    @abc.abstractmethod
+    def execute(self, sql, params=None):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def execute_many(self, sql, params=None):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def mogrify(self, sql, params=None):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def fetchone(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def fetchmany(self, size: int = None):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def fetchall(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def scroll(self, value: int, mode: Literal["relative"] = "relative"):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    @property
+    def description(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    @property
+    def rownumber(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    @property
+    def rowcount(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def close(self):
         raise NotImplementedError
